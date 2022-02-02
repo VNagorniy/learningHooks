b14014e93fc8c57bff49dec4735dded3710500ad
Doom <vnagorniyrnd@gmail.com>
2022-02-02 15:40:22 +0300

    знакомство с useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component, useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
@@ -60,19 +60,13 @@ const Slider = (props) => {
   const [slide, setSlide] = useState(0);
   const [autoplay, setAutoplay] = useState(false);
 
-  function logging() {
-    console.log('log');
-  }
-  useEffect(() => {
-    console.log('effect');
-    document.title = `Slide: ${slide}`;
-
-    window.addEventListener('click', logging);
-
-    return () => {
-      window.removeEventListener('click', logging);
-    };
-  }, [slide]);
+  const getSomeImages = useCallback(() => {
+    console.log('fetching');
+    return [
+      'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg',
+      'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg',
+    ];
+  }, []);
 
   function changeSlide(i) {
     setSlide(slide + i);
@@ -85,7 +79,8 @@ const Slider = (props) => {
   return (
     <Container>
       <div className='slider w-50 m-auto'>
-        <img className='d-block w-100' src='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg' alt='slide' />
+        <Slide getSomeImages={getSomeImages} />
+
         <div className='text-center mt-5'>
           Active slide {slide} <br /> {autoplay ? 'auto' : null}
         </div>
@@ -102,6 +97,20 @@ const Slider = (props) => {
         </div>
       </div>
     </Container>
+  );
+};
+
+const Slide = ({ getSomeImages }) => {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    setImages(getSomeImages());
+  }, [getSomeImages]);
+  return (
+    <>
+      {images.map((url, i) => (
+        <img key={i} className='d-block w-100' src={url} alt='slide' />
+      ))}
+    </>
   );
 };
 
