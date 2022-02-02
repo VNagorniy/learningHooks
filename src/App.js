1943b1ba9d1e5ffef0c6428f6938fccc0efe0846
Doom <vnagorniyrnd@gmail.com>
2022-02-02 19:53:14 +0300

    знакомство с useMemo

--- src/App.js
+++ src/App.js
@@ -1,60 +1,11 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, useMemo } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
-// class Slider extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       autoplay: false,
-//       slide: 0,
-//     };
-//   }
-
-//   componentDidMount() {
-//     document.title = `Slide: ${this.state.slide}`;
-//   }
-
-//   componentDidUpdate() {
-//     document.title = `Slide: ${this.state.slide}`;
-//   }
-
-//   changeSlide = (i) => {
-//     this.setState(({ slide }) => ({
-//       slide: slide + i,
-//     }));
-//   };
-
-//   toggleAutoplay = () => {
-//     this.setState(({ autoplay }) => ({
-//       autoplay: !autoplay,
-//     }));
-//   };
-
-//   render() {
-//     return (
-//       <Container>
-//         <div className='slider w-50 m-auto'>
-//           <img className='d-block w-100' src='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg' alt='slide' />
-//           <div className='text-center mt-5'>
-//             Active slide {this.state.slide} <br /> {this.state.autoplay ? 'auto' : null}
-//           </div>
-//           <div className='buttons mt-3'>
-//             <button className='btn btn-primary me-2' onClick={() => this.changeSlide(-1)}>
-//               -1
-//             </button>
-//             <button className='btn btn-primary me-2' onClick={() => this.changeSlide(1)}>
-//               +1
-//             </button>
-//             <button className='btn btn-primary me-2' onClick={this.toggleAutoplay}>
-//               toggle autoplay
-//             </button>
-//           </div>
-//         </div>
-//       </Container>
-//     );
-//   }
-// }
+const countTotal = (num) => {
+  console.log('counting...');
+  return num + 10;
+};
 
 const Slider = (props) => {
   const [slide, setSlide] = useState(0);
@@ -66,7 +17,7 @@ const Slider = (props) => {
       'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg',
       'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg',
     ];
-  }, []);
+  }, [slide]);
 
   function changeSlide(i) {
     setSlide(slide + i);
@@ -76,13 +27,31 @@ const Slider = (props) => {
     setAutoplay(!autoplay);
   }
 
+  const total = useMemo(() => {
+    return countTotal(slide);
+  }, [slide]);
+
+  const style = useMemo(
+    () => ({
+      color: slide > 4 ? 'red' : 'black',
+    }),
+    [slide]
+  );
+
+  useEffect(() => {
+    console.log('styles!');
+  }, [style]);
+
   return (
     <Container>
       <div className='slider w-50 m-auto'>
         <Slide getSomeImages={getSomeImages} />
-
         <div className='text-center mt-5'>
           Active slide {slide} <br /> {autoplay ? 'auto' : null}
+        </div>
+        <div style={style} className='text-center mt-5'>
+          Total slides:{total}
+          <br />
         </div>
         <div className='buttons mt-3'>
           <button className='btn btn-primary me-2' onClick={() => changeSlide(-1)}>
