17a8a63e25380028b0c236d122560e27a2fbdfc1
Doom <vnagorniyrnd@gmail.com>
2022-02-09 16:55:57 +0300

    знакомство с React.memo и React.PureComponent

--- src/App.js
+++ src/App.js
@@ -1,49 +1,78 @@
-import { useRef, useState, useEffect } from 'react';
+import { useState, memo, PureComponent } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
-function useInputWithValidate(initialValue) {
-  const [value, setValue] = useState(initialValue);
+class Form extends PureComponent {
+  render() {
+    console.log('render');
 
-  const onChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  const validateInput = () => {
-    return value.search(/\d/) >= 0;
-  };
-
-  return { value, onChange, validateInput };
+    return (
+      <Container>
+        <form className='w-50 border mt-5 p-3 m-auto'>
+          <div className='mb-3'>
+            <label htmlFor='exampleFormControlInput1' className='form-label mt-3'>
+              Email address
+            </label>
+            <input value={this.props.mail} type='email' className='form-control' id='exampleFormControlInput1' placeholder='name@example.com' />
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='exampleFormControlTextarea1' className='form-label'>
+              Example textarea
+            </label>
+            <textarea value={this.props.text} className='form-control' id='exampleFormControlTextarea1' rows='3'></textarea>
+          </div>
+        </form>
+      </Container>
+    );
+  }
 }
-const Form = () => {
-  const input = useInputWithValidate('');
-  const textArea = useInputWithValidate('');
 
-  const color = input.validateInput() ? 'text-danger' : null;
+// function propsCompare(prevProps, nextProps) {
+//   return prevProps.mail.name === nextProps.mail.name && prevProps.text === nextProps.text;
+// }
 
-  return (
-    <Container>
-      <form className='w-50 border mt-5 p-3 m-auto'>
-        <div className='mb-3'>
-          <input value={`${input.value} / ${textArea.value}`} type='text' className='form-control' readOnly />
-          <label htmlFor='exampleFormControlInput1' className='form-label mt-3'>
-            Email address
-          </label>
-          <input onChange={input.onChange} type='email' value={input.value} className={`form-control ${color}`} id='exampleFormControlInput1' placeholder='name@example.com' />
-        </div>
-        <div className='mb-3'>
-          <label htmlFor='exampleFormControlTextarea1' className='form-label'>
-            Example textarea
-          </label>
-          <textarea onChange={textArea.onChange} value={textArea.value} className='form-control' id='exampleFormControlTextarea1' rows='3'></textarea>
-        </div>
-      </form>
-    </Container>
-  );
-};
+// const Form = memo((props) => {
+//   return (
+//     <Container>
+//       <form className='w-50 border mt-5 p-3 m-auto'>
+//         <div className='mb-3'>
+//           <label htmlFor='exampleFormControlInput1' className='form-label mt-3'>
+//             Email address
+//           </label>
+//           <input value={props.mail} type='email' className='form-control' id='exampleFormControlInput1' placeholder='name@example.com' />
+//         </div>
+//         <div className='mb-3'>
+//           <label htmlFor='exampleFormControlTextarea1' className='form-label'>
+//             Example textarea
+//           </label>
+//           <textarea value={props.text} className='form-control' id='exampleFormControlTextarea1' rows='3'></textarea>
+//         </div>
+//       </form>
+//     </Container>
+//   );
+// }, propsCompare);
 
 function App() {
-  return <Form />;
+  const [data, setData] = useState({
+    mail: 'name@example.com',
+    text: 'some text',
+  });
+
+  return (
+    <>
+      <Form mail={data.mail} text={data.text} />
+      <button
+        onClick={() =>
+          setData({
+            mail: 'second@example.com',
+            text: 'another text',
+          })
+        }
+      >
+        Click me
+      </button>
+    </>
+  );
 }
 
 export default App;
