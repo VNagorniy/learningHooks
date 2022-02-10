8a172c66a0147b2123b09b9a3badffbcef0f1c86
Doom <vnagorniyrnd@gmail.com>
2022-02-10 23:51:14 +0300

    знакомство с React Context, useContext

--- src/App.js
+++ src/App.js
@@ -1,56 +1,21 @@
-import { useState, memo, PureComponent } from 'react';
-import { Container } from 'react-bootstrap';
+import { useState } from 'react';
 import './App.css';
+import Form from './Form';
+import dataContext from './context';
 
-class Form extends PureComponent {
-  render() {
-    console.log('render');
+const { Provider } = dataContext;
 
-    return (
-      <Container>
-        <form className='w-50 border mt-5 p-3 m-auto'>
-          <div className='mb-3'>
-            <label htmlFor='exampleFormControlInput1' className='form-label mt-3'>
-              Email address
-            </label>
-            <input value={this.props.mail} type='email' className='form-control' id='exampleFormControlInput1' placeholder='name@example.com' />
-          </div>
-          <div className='mb-3'>
-            <label htmlFor='exampleFormControlTextarea1' className='form-label'>
-              Example textarea
-            </label>
-            <textarea value={this.props.text} className='form-control' id='exampleFormControlTextarea1' rows='3'></textarea>
-          </div>
-        </form>
-      </Container>
-    );
-  }
-}
-
-// function propsCompare(prevProps, nextProps) {
-//   return prevProps.mail.name === nextProps.mail.name && prevProps.text === nextProps.text;
+// class InputComponent extends Component {
+//   render() {
+//     return (
+//       <Consumer>
+//         {(value) => {
+//           return <input value={value.mail} type='email' className='form-control' id='exampleFormControlInput1' placeholder='name@example.com' />;
+//         }}
+//       </Consumer>
+//     );
+//   }
 // }
-
-// const Form = memo((props) => {
-//   return (
-//     <Container>
-//       <form className='w-50 border mt-5 p-3 m-auto'>
-//         <div className='mb-3'>
-//           <label htmlFor='exampleFormControlInput1' className='form-label mt-3'>
-//             Email address
-//           </label>
-//           <input value={props.mail} type='email' className='form-control' id='exampleFormControlInput1' placeholder='name@example.com' />
-//         </div>
-//         <div className='mb-3'>
-//           <label htmlFor='exampleFormControlTextarea1' className='form-label'>
-//             Example textarea
-//           </label>
-//           <textarea value={props.text} className='form-control' id='exampleFormControlTextarea1' rows='3'></textarea>
-//         </div>
-//       </form>
-//     </Container>
-//   );
-// }, propsCompare);
 
 function App() {
   const [data, setData] = useState({
@@ -59,8 +24,8 @@ function App() {
   });
 
   return (
-    <>
-      <Form mail={data.mail} text={data.text} />
+    <Provider value={data}>
+      <Form text={data.text} />
       <button
         onClick={() =>
           setData({
@@ -71,7 +36,7 @@ function App() {
       >
         Click me
       </button>
-    </>
+    </Provider>
   );
 }
 
---
+++ src/Input.js
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import dataContext from './context';
+
+const InputComponent = () => {
+  const context = useContext(dataContext);
+  return <input value={context.mail} type='email' className='form-control' id='exampleFormControlInput1' placeholder='name@example.com' />;
+};
+
+export default InputComponent;
